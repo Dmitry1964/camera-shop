7d8ef0f18c0c3d1e9c16f593e1d4b78b300aba26
Dmytry64 <nosdm64@gmail.com>
2023-09-30 19:29:33 +0300

    Добавляет баннер и слайдер

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,23 +1,45 @@
-import {useEffect} from 'react';
-import { useAppDispatch } from '../../hooks/hooks';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { fetchPromoOffersList } from '../../store/api-actons';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 const Banner = (): JSX.Element => {
 
   const dispatch = useAppDispatch();
+  const promoOffersList = useAppSelector((state) => state.promoOffersList);
 
   useEffect(() => {
     dispatch(fetchPromoOffersList());
-  })
-  const a = 3;
+  }, [dispatch]);
+
   return (
-    <div className="banner">
-      <picture>
-        <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x" />
-        <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер" />
-      </picture>
-      <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
-    </div>
+    <Swiper
+      modules={[Autoplay]}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+    >
+      {Object.values(promoOffersList).map((item) => (
+        <SwiperSlide key={item.id}>
+          <div className="banner">
+            <picture>
+              <source type="image/webp" srcSet={`${item.previewImgWebp}, ${item.previewImgWebp2x}`} />
+              <img src={item.previewImg} srcSet={item.previewImg2x} width="1280" height="280" alt="баннер" />
+            </picture>
+            <p className="banner__info">
+              <span className="banner__message">Новинка!</span>
+              <span className="title title--h1">{item.name}</span>
+              <span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
+              <a className="btn" href="#">Подробнее</a>
+            </p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
 
   );
 };
